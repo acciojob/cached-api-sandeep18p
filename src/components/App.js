249a11ad7c59a996e673cffed84f44f0cf7f9ea3
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-14 18:49:04 +0530

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect,useMemo } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [data, setData] = useState(null);
+  const [loading,setLoading] = useState(true);
 
-export default App
+  useEffect(() => {
+    const fetchData = () => {
+      
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response)=>response.json())
+        .then((jsonData)=>{
+          setData(jsonData);
+          setLoading(false);
+          console.log(jsonData);
+        }).catch(error => {
+          console.error('Error fetching data:', error);
+          setLoading(false);
+        });
+
+    };
+
+    fetchData();
+  }, []);
+
+  const memoizedData = useMemo(() => data, [data]);
+
+  return (
+    <div className="App">
+     {  loading ? <p>loading....</p>: <div>
+          <h2>Posts</h2>
+          <ul>
+            {memoizedData && memoizedData.map(post => (
+              <li key={post.id}>
+                <h4>{post.title}</h4>
+                <p>{post.body}</p>
+              </li>
+            ))}
+          </ul>
+        </div> }
+        
+   
+    </div>
+  );
+};
+
+export default App;
